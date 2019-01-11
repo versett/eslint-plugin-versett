4d7ef9653baf890c8807904bb9216cab1318b4a2
Soroush Karimi <sorooshk42@gmail.com>
2019-01-11 16:03:57 +0330

    feat(styleRules): add node, comment, style and variable rules (#15)

---
+++ rules/eslint/variables/on.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  rules: {
+    // require or disallow initialization in variable declarations
+    "init-declarations": 0,
+    // disallow deleting variables
+    "no-delete-var": 0,
+    // disallow labels that share a name with a variable
+    "no-label-var": 0,
+    // disallow specified global variables
+    "no-restricted-globals": 0,
+    // disallow variable declarations from shadowing variables declared in the outer scope
+    "no-shadow": 0,
+    // disallow identifiers from shadowing restricted names
+    "no-shadow-restricted-names": 0,
+    // disallow the use of undeclared variables unless mentioned in /*global */ comments
+    "no-undef": 0,
+    // disallow initializing variables to undefined
+    "no-undef-init": 0,
+    // disallow the use of undefined as an identifier
+    "no-undefined": 0,
+    // disallow unused variables
+    "no-unused-vars": 0,
+    // disallow the use of variables before they are defined
+    "no-use-before-define": 0
+  }
+};
