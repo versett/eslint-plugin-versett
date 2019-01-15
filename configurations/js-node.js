8e7a71d80f27351fd18af7f036e7cfd143a61424
Soroush Karimi <sorooshk42@gmail.com>
2019-01-16 01:07:02 +0330

    modify js-node and js-react configs (#24)

--- configurations/js-node.js
+++ configurations/js-node.js
@@ -1,6 +1,8 @@
 "use strict";
 
 module.exports = {
+  parser: "babel-eslint",
+  plugins: ["import"],
   extends: ["versett/configurations/eslint-all", "versett/rules/jest/on"],
   env: {
     es6: true,
@@ -10,5 +12,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
     sourceType: "module"
-  }
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js"]
+      }
+    }
+  },
+  rules: {}
 };
---
+++ configurations/js-react.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  parser: "babel-eslint",
+  plugins: ["jsx-a11y", "import"],
+  extends: [
+    "versett/configurations/eslint-all",
+    "versett/rules/jest/on",
+    "versett/rules/react/on",
+    "plugin:jsx-a11y/recommended"
+  ],
+  env: {
+    es6: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"]
+      }
+    }
+  },
+  rules: {}
+};
